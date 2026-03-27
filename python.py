875a3ed5256dfac6ed4b4a0cbce4de09c56d2d8c
Priti2707 <pritisingh272004@gmail.com>
2026-03-27 23:32:42 +0530

    first commit on main

---
+++ python.py
@@ -0,0 +1,22 @@
+subjects = {}
+
+n = int(input("Enter number of subjects: "))
+
+for i in range(n):
+    name = input("Subject name: ")
+    marks = float(input("Marks: "))
+    subjects[name] = marks
+
+total_time = float(input("Enter daily study hours: "))
+
+total_inverse = sum(100 - m for m in subjects.values())
+
+print("\n Smart Study Plan:\n")
+
+for sub, marks in subjects.items():
+    weight = (100 - marks) / total_inverse
+    time_alloc = weight * total_time
+    print(f"{sub}: {round(time_alloc,2)} hours")
+
+weak = min(subjects, key=subjects.get)
+print(f"\n Focus more on: {weak}")
