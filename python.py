1797bd8e8eff8b1eab52365e0b0855114aa885e5
Priti <pritisingh272004@gmail.com>
2026-07-16 00:24:32 +0530

    Update python.py

--- python.py
+++ python.py
@@ -9,12 +9,12 @@ for i in range(n):
 
 total_time = float(input("Enter daily study hours: "))
 
-total_inverse = sum(100 - m for m in subjects.values())
+total_inverse = sum(10 - m for m in subjects.values())
 
 print("\n Smart Study Plan:\n")
 
 for sub, marks in subjects.items():
-    weight = (100 - marks) / total_inverse
+    weight = (10 - marks) / total_inverse
     time_alloc = weight * total_time
     print(f"{sub}: {round(time_alloc,2)} hours")
 
